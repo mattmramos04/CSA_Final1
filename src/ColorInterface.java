fc0a645f3e41771531d3b0ddfa859ba34d16d6c3
Matthew Ramos <92108372+mattmramos04@users.noreply.github.com>
2021-12-14 08:04:03 -0800

    Update ColorInterface.java

--- src/ColorInterface.java
+++ src/ColorInterface.java
@@ -4,6 +4,5 @@ interface ColorInterface
     String TEXT_GREEN = "\u001B[32m"; //green
     String TEXT_RESET = "\u001B[0m"; //black/default color
 
-    //I believe this is a method
-    String isItColored();
+   
 }
